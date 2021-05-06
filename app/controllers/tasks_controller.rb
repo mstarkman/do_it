f30a0767f751fa14d1b7ffd6050725cd619385c8
Mark Starkman <me@markstarkman.com>
2021-05-06 09:54:15 -0400

    Allow add and destroy

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,31 @@
 class TasksController < ApplicationController
+  layout false, only: [:create]
   def index
     @tasks = Task.all.order(created_at: :desc)
+  end
+
+  def create
+    task = Task.create!(task_params)
+
+    respond_to do |format|
+      # format.turbo_stream { render turbo_stream: turbo_stream.prepend(:tasks, task) }
+      format.html { redirect_to root_url }
+    end
+  end
+
+  def destroy
+    task = Task.find(params[:id])
+    task.destroy!
+
+    respond_to do |format|
+      format.turbo_stream { render turbo_stream: turbo_stream.remove(task) }
+      format.html         { redirect_to root_url }
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:body)
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,5 @@
 class Task < ApplicationRecord
+  after_create_commit { broadcast_prepend_to :tasks }
+  after_update_commit { broadcast_replace_to :tasks }
+  after_destroy_commit { broadcast_remove_to :tasks }
 end
