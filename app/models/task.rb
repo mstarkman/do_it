6921ec3367cbbc24a5ebda89cde6d06b67497763
Mark Starkman <me@markstarkman.com>
2021-05-10 21:47:14 -0400

    Broadcast complete all tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,15 +1,36 @@
 class Task < ApplicationRecord
-  after_create_commit { broadcast_prepend_to :tasks }
-  after_update_commit { broadcast_replace_to :tasks }
-  after_destroy_commit { broadcast_remove_to :tasks }
+  after_create_commit :prepend_task
+  after_update_commit :replace_task
+  after_destroy_commit :remove_task
 
   scope :incomplete, ->{ where(complete: false) }
 
   def self.all_complete?
-    self.incomplete.count == 0
+    Task.count > 0 && self.incomplete.count == 0
   end
 
   def complete!
     update!(complete: true)
+  end
+
+  private
+
+  def prepend_task
+    broadcast_prepend_to :tasks
+    replace_complete_all_tasks
+  end
+
+  def replace_task
+    broadcast_replace_to :tasks
+    replace_complete_all_tasks
+  end
+
+  def remove_task
+    broadcast_remove_to :tasks
+    replace_complete_all_tasks
+  end
+
+  def replace_complete_all_tasks
+    Turbo::StreamsChannel.broadcast_replace_to :tasks, target: "complete_all_tasks", partial: 'tasks/complete_all', locals: { all_complete: Task.all_complete? }
   end
 end
